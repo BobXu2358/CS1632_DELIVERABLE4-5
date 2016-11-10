8010dc4bd02f75e68984db7c25a0948dd8725e2d
BobXu2358 <xxh2358@gmail.com>
2016-11-10 03:01:36 -0500

    pinning test

---
+++ MonkeySimTest.java
@@ -0,0 +1,66 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.*;
+import java.util.*;
+
+//pinning test
+
+public class MonkeySimTest {
+
+	@Test
+	public void testStringify1() {
+		String str = "//Round 1: Threw banana from Monkey (#32 / ID 223524) to Monkey (#16 / ID 223508)";
+		Monkey m1 = Mockito.mock(Monkey.class);
+		Monkey m2 = Mockito.mock(Monkey.class);
+		Mockito.when(m1.getMonkeyNum()).thenReturn(32);
+		Mockito.when(m2.getMonkeyNum()).thenReturn(16);
+		try{ 
+			Mockito.when(m1.getId()).thenReturn(223524);
+			Mockito.when(m2.getId()).thenReturn(223508);
+		}
+		catch (NoIdException noidex) {
+		    System.out.println("INVALID MONKEY!");
+		    System.exit(2);
+		}
+			assertEquals(str, MonkeySim.stringifyResults(1, m1, m2));
+	}
+
+	@Test
+	public void testStringify2() {
+		String str = "//Round 3: Threw banana from Monkey (#23 / ID 223515) to Monkey (#70 / ID 223562)";
+		Monkey m1 = Mockito.mock(Monkey.class);
+		Monkey m2 = Mockito.mock(Monkey.class);
+		Mockito.when(m1.getMonkeyNum()).thenReturn(23);
+		Mockito.when(m2.getMonkeyNum()).thenReturn(70);
+		try{
+			Mockito.when(m1.getId()).thenReturn(223515);
+			Mockito.when(m2.getId()).thenReturn(223562);
+		}
+		catch (NoIdException noidex) {
+		    System.out.println("INVALID MONKEY!");
+		    System.exit(2);
+		}
+			assertEquals(str, MonkeySim.stringifyResults(3, m1, m2));
+	}
+	
+	@Test
+	public void testStringify3() {
+		String str = "//Round 11: Threw banana from Monkey (#20 / ID 223512) to Monkey (#10 / ID 223502)";
+		Monkey m1 = Mockito.mock(Monkey.class);
+		Monkey m2 = Mockito.mock(Monkey.class);
+		Mockito.when(m1.getMonkeyNum()).thenReturn(20);
+		Mockito.when(m2.getMonkeyNum()).thenReturn(10);
+		try{ 
+			Mockito.when(m1.getId()).thenReturn(223512);
+			Mockito.when(m2.getId()).thenReturn(223502);
+		}
+		catch (NoIdException noidex) {
+		    System.out.println("INVALID MONKEY!");
+		    System.exit(2);
+		}
+			assertEquals(str, MonkeySim.stringifyResults(11, m1, m2));
+	}
+	
+	}
+	
+}
---
+++ MonkeyTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+//pinning test
+
+public class MonkeyTest {
+
+	private Monkey m = new Monkey();
+	
+	@Test
+	public void testGenerateIdWith4() {
+		assertEquals(223496, m.generateId(4));
+	}
+	
+	@Test
+	public void testGenerateIdWith16() {
+		assertEquals(223508, m.generateId(16));
+	}
+	
+	@Test
+	public void testGenerateIdWith40() {
+		assertEquals(223532, m.generateId(40));
+	}
+}
