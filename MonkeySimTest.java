f8e0bf6f63f64461f17bafbb0ded6bb198c309ef
Xu <xix35@dlh-m31.labs.pitt.edu>
2016-11-10 21:22:46 -0500

    updated pinning tests

--- MonkeySimTest.java
+++ MonkeySimTest.java
@@ -14,14 +14,11 @@ public class MonkeySimTest {
 		Monkey m2 = Mockito.mock(Monkey.class);
 		Mockito.when(m1.getMonkeyNum()).thenReturn(32);
 		Mockito.when(m2.getMonkeyNum()).thenReturn(16);
-		try{ 
+		try{
 			Mockito.when(m1.getId()).thenReturn(223524);
 			Mockito.when(m2.getId()).thenReturn(223508);
 		}
-		catch (NoIdException noidex) {
-		    System.out.println("INVALID MONKEY!");
-		    System.exit(2);
-		}
+		catch (NoIdException noidex) {}
 			assertEquals(str, MonkeySim.stringifyResults(1, m1, m2));
 	}
 
@@ -36,13 +33,10 @@ public class MonkeySimTest {
 			Mockito.when(m1.getId()).thenReturn(223515);
 			Mockito.when(m2.getId()).thenReturn(223562);
 		}
-		catch (NoIdException noidex) {
-		    System.out.println("INVALID MONKEY!");
-		    System.exit(2);
-		}
+		catch (NoIdException noidex) {}
 			assertEquals(str, MonkeySim.stringifyResults(3, m1, m2));
 	}
-	
+
 	@Test
 	public void testStringify3() {
 		String str = "//Round 11: Threw banana from Monkey (#20 / ID 223512) to Monkey (#10 / ID 223502)";
@@ -50,16 +44,56 @@ public class MonkeySimTest {
 		Monkey m2 = Mockito.mock(Monkey.class);
 		Mockito.when(m1.getMonkeyNum()).thenReturn(20);
 		Mockito.when(m2.getMonkeyNum()).thenReturn(10);
-		try{ 
+		try{
 			Mockito.when(m1.getId()).thenReturn(223512);
 			Mockito.when(m2.getId()).thenReturn(223502);
 		}
-		catch (NoIdException noidex) {
-		    System.out.println("INVALID MONKEY!");
-		    System.exit(2);
-		}
+		catch (NoIdException noidex) {}
 			assertEquals(str, MonkeySim.stringifyResults(11, m1, m2));
 	}
-	
-	
+
+	@Test
+	public void testGetFirstMonkey1() {
+		List<Monkey> ml = new LinkedList<Monkey>();
+		Monkey m1 = Mockito.mock(Monkey.class);
+		Monkey m2 = Mockito.mock(Monkey.class);
+		Monkey m3 = Mockito.mock(Monkey.class);
+		Mockito.when(m1.getMonkeyNum()).thenReturn(1);
+		Mockito.when(m2.getMonkeyNum()).thenReturn(2);
+		Mockito.when(m3.getMonkeyNum()).thenReturn(3);
+		ml.add(m1);
+		ml.add(m2);
+		ml.add(m3);
+		assertEquals(m1, MonkeySim.getFirstMonkey(ml));
+	}
+
+	@Test
+	public void testGetFirstMonkey2() {
+		List<Monkey> ml = new LinkedList<Monkey>();
+		Monkey m1 = Mockito.mock(Monkey.class);
+		Monkey m2 = Mockito.mock(Monkey.class);
+		Monkey m3 = Mockito.mock(Monkey.class);
+		Mockito.when(m1.getMonkeyNum()).thenReturn(2);
+		Mockito.when(m2.getMonkeyNum()).thenReturn(1);
+		Mockito.when(m3.getMonkeyNum()).thenReturn(3);
+		ml.add(m1);
+		ml.add(m2);
+		ml.add(m3);
+		assertEquals(m1, MonkeySim.getFirstMonkey(ml));
+	}
+
+	@Test
+	public void testGetFirstMonkey3() {
+		List<Monkey> ml = new LinkedList<Monkey>();
+		Monkey m1 = Mockito.mock(Monkey.class);
+		Monkey m2 = Mockito.mock(Monkey.class);
+		Monkey m3 = Mockito.mock(Monkey.class);
+		Mockito.when(m1.getMonkeyNum()).thenReturn(2);
+		Mockito.when(m2.getMonkeyNum()).thenReturn(3);
+		Mockito.when(m3.getMonkeyNum()).thenReturn(1);
+		ml.add(m1);
+		ml.add(m2);
+		ml.add(m3);
+		assertEquals(m3, MonkeySim.getFirstMonkey(ml));
+	}
 }
--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Project under test: Monkey Simulator<br>
 Group Members: Vivien Chang, Xinhai Xu
 
 ## Instruction for running pinning test
+- First, have these three jar files in folder:  1. junit-4.12.jar; 2. hamcrest-core-1.3.jar; 3. mockito-all-1.9.5.jar
 - For MacOs or Linux: </br>Compile with: javac -cp .:./junit-4.12.jar:./hamcrest-core-1.3.jar:./mockito-all-1.9.5.jar *.java</br>Run with: java -cp .:./junit-4.12.jar:./hamcrest-core-1.3.jar:./mockito-all-1.9.5.jar TestRunner
 - For Windows: </br>Compile with: javac -cp ".;./junit-4.12.jar;./hamcrest-core-1.3.jar;./mockito-all-1.9.5.jar" *.java</br>Run with: java -cp ".;./junit-4.12.jar;./hamcrest-core-1.3.jar;./mockito-all-1.9.5.jar" TestRunner
