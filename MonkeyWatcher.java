f0d049829d82026e3ff1f69432d5a6037ef39a81
BobXu2358 <xxh2358@gmail.com>
2016-11-21 23:52:52 -0500

    roll back unnecessary changes to original code

--- MonkeyWatcher.java
+++ MonkeyWatcher.java
@@ -15,8 +15,8 @@ public class MonkeyWatcher {
    * Increment number of rounds.
    */
 
-  public int incrementRounds() {
-    return numRounds++;
+  public void incrementRounds() {
+    numRounds++;
   }
 
 }
